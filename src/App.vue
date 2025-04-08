b4385642d425b21f55d6ee93a8e3a21c6a233c8c
Arch <a.mallorca.psa@gmail.com>
2025-04-08 13:15:26 +0800

    Web dev

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+
+<template>
+<SignupFormVue/>
+
+ 
+</template>
+
+<script> 
+import SignupFormVue from './components/SignupForm.vue'
+
+export default
+{ components:{
+  SignupFormVue
+}
+
+}
+</script>
+
+<style>
+body {
+ margin: 0;
+ Background: #eee;
+}
+
+</style>
